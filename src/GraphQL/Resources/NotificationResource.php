10dc32e2d0237cd4080e070a6da4426afb71db52
Jake Booher <jake.booher@gmail.com>
2021-11-29 14:31:44 -0600

    Fix notification content type

--- src/GraphQL/Resources/NotificationResource.php
+++ src/GraphQL/Resources/NotificationResource.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Audentio\LaravelNotifications\GraphQL\Resources;
 
 use App\Models\Notification;
+use Audentio\LaravelBase\Utils\ContentTypeUtil;
 use Audentio\LaravelGraphQL\GraphQL\Definitions\Type;
 use Audentio\LaravelGraphQL\GraphQL\Support\Resource as GraphQLResource;
+use Audentio\LaravelNotifications\Models\Interfaces\NotificationModelInterface;
 use GraphQL;
 
 class NotificationResource extends GraphQLResource
@@ -23,7 +25,12 @@ class NotificationResource extends GraphQLResource
             'sender' => [
                 'type' => GraphQL::type('User'),
             ],
-            'content_type' => ['type' => GraphQL::type('NotificationContentTypeEnum')],
+            'content_type' => [
+                'type' => GraphQL::type('NotificationContentTypeEnum'),
+                'resolve' => function (NotificationModelInterface $notificationModel) {
+                    return ContentTypeUtil::getFriendlyContentTypeName($notificationModel->content_type);
+                }
+            ],
             'content_id' => ['type' => Type::id()],
             'content' => ['type' => GraphQL::type('NotificationContent')],
             'message' => Type::methodValue(Type::string(), 'getMessage', [
