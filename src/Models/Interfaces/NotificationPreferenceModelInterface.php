b5669e84c89307c804d1f1d38ffa0775611715a9
Jake Booher <jake.booher@gmail.com>
2023-10-25 09:10:16 +0100

    Add a should_display_callback for notification preferences

--- src/Models/Interfaces/NotificationPreferenceModelInterface.php
+++ src/Models/Interfaces/NotificationPreferenceModelInterface.php
@@ -3,9 +3,13 @@
 namespace Audentio\LaravelNotifications\Models\Interfaces;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Collection;
 
 interface NotificationPreferenceModelInterface
 {
     public function notificationPreferenceGroup(): BelongsTo;
+    public function getDefaultUserNotificationPreferenceValue(): array;
     public function getName(): string;
+    public static function getCached(): Collection;
+    public function shouldDisplay(): bool;
 }
