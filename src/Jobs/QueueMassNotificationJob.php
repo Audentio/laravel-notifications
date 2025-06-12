c84ae20810aa6ec079db116e822161c15091c9e9
Jake Booher <jake.booher@gmail.com>
2025-06-12 10:45:10 -0500

    Split mass notification queues into batches

--- src/Jobs/QueueMassNotificationJob.php
+++ src/Jobs/QueueMassNotificationJob.php
@@ -3,12 +3,17 @@
 namespace Audentio\LaravelNotifications\Jobs;
 
 use App\Core;
+use App\Models\User;
 use Audentio\LaravelNotifications\Notifications\AbstractNotification;
 use Audentio\LaravelNotifications\Notifications\Interfaces\MassNotificationInterface;
+use Audentio\LaravelNotifications\Utils\TimerUtil;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Shopify\Rest\Admin2022_04\Collect;
 
 class QueueMassNotificationJob implements ShouldQueue
 {
@@ -19,20 +24,46 @@ class QueueMassNotificationJob implements ShouldQueue
     /** @var MassNotificationInterface|AbstractNotification */
     protected MassNotificationInterface $notification;
 
+    /** @var Collection|User[] */
+    protected Collection $remainingUsers;
+
+    protected int $limit = 100;
+
     public function handle(): void
     {
-        $users = $this->notification->getUsers();
+        $remainingUsers = $this->remainingUsers;
 
-        try {
-            \Notification::send($users, $this->notification);
-        } catch (\Throwable $e) {
-            // Intentionally ignoring errors here so notifications don't resend.
-            Core::captureException($e);
+        $timer = new TimerUtil(45);
+
+        $count = 0;
+        foreach ($remainingUsers as $key => $user) {
+            $count++;
+            $remainingUsers->forget($key);
+            try {
+                \Notification::send($user, $this->notification);
+            } catch (\Throwable $e) {
+                // Intentionally ignoring errors here so notifications don't resend.
+                Core::captureException($e);
+            }
+            if ($count > $this->limit || $timer->isLimitReached()) {
+                break;
+            }
         }
+
+        if ($remainingUsers->isEmpty()) {
+            return;
+        }
+        QueueMassNotificationJob::dispatch($this->notification, $remainingUsers);
     }
 
-    public function __construct(MassNotificationInterface $notification)
+    public function __construct(MassNotificationInterface $notification, ?Collection $remainingUsers = null)
     {
         $this->notification = $notification;
+
+        if ($remainingUsers) {
+            $this->remainingUsers = $remainingUsers;
+        } else {
+            $this->remainingUsers = $this->notification->getUsers();
+        }
     }
 }
