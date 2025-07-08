bc5ac5ff2942b125c0648f7eb30e72cb46ead4b3
Jake Booher <jake.booher@gmail.com>
2025-07-08 14:44:03 -0500

    Fix definition

--- src/Notifications/AbstractNotification.php
+++ src/Notifications/AbstractNotification.php
@@ -54,7 +54,7 @@ abstract class AbstractNotification extends Notification
         return null;
     }
 
-    public function getUrl(NotificationModelInterface $notification, $notifiable): ?string
+    public function getUrl(?NotificationModelInterface $notification, $notifiable): ?string
     {
         return null;
     }
@@ -122,6 +122,6 @@ abstract class AbstractNotification extends Notification
 
     abstract public function getNotificationPreferenceId(): ?string;
     abstract public function getKind(): string;
-    abstract public function getNotificationMessage(NotificationModelInterface $notification, $notifiable): ?string;
+    abstract public function getNotificationMessage(?NotificationModelInterface $notification, $notifiable): ?string;
     abstract public function getContent(): ?AbstractModel;
 }
