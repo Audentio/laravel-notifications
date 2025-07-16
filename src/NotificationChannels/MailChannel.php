e473f1c177a9d29ec62ed273ead40b17ad788d62
Jake Booher <jake.booher@gmail.com>
2025-07-16 11:00:15 -0500

    Fix MailChannel return type

--- src/NotificationChannels/MailChannel.php
+++ src/NotificationChannels/MailChannel.php
@@ -4,23 +4,25 @@ namespace Audentio\LaravelNotifications\NotificationChannels;
 
 use Audentio\LaravelNotifications\Models\Interfaces\NotifiableUserInterface;
 use Audentio\LaravelNotifications\Notifications\AbstractNotification;
+use Illuminate\Mail\SentMessage;
 use Illuminate\Notifications\Channels\MailChannel as BaseMailChannel;
 use Illuminate\Notifications\Notification;
 
 class MailChannel extends BaseMailChannel
 {
-    public function send($notifiable, Notification $notification): void
+    public function send($notifiable, Notification $notification): ?SentMessage
     {
         if ($notification instanceof AbstractNotification
             && $notifiable instanceof NotifiableUserInterface
             && !$notification->shouldBypassEmailVerificationCheck($notifiable)
             && !$notifiable->isEmailVerified()
         ) {
-            return;
+            return null;
         }
+
         $message = $notification->toMail($notifiable);
         if ($message === null) {
-            return;
+            return null;
         }
 
         parent::send($notifiable, $notification);
